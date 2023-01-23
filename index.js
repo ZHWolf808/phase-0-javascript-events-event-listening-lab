99be3fe6fa087308c7aa7da356747555b4f177a7
Zachary Wolf <wolfislnd8@gmail.com>
2023-01-23 11:01:46 -0500

    commit tree 12c55c7b6f016e9d7e97730384f439c8d343258b

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+ const input = document.getElementById('button');
+ console.log("input",input)
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
